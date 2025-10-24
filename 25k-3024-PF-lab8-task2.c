ed1f1814109053d9f8bb899dd562ba9aea7756a3
Ilham-Student-25K-3024 <k253024@nu.edu.pk>
2025-10-23 22:28:26 -0700

    Add files via upload

--- 25k-3024-PF-lab8-task2.c
+++ 25k-3024-PF-lab8-task2.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main(){
 	int matrix[3][4]={{10,22,35,41},{50,65,73,80},{91,10,11,12}};
-	int i,j,max=0;
+	int i,j,max=0,index;
 	for(i=0;i<3;i++){
 		for(j=0;j<4;j++){
 			printf("\t%d",matrix[i][j]);
 			if(max<matrix[i][j]){
 				max=matrix[i][j];
+				index=i;
 			}
 		}
 		printf("\n");
 		
 	}printf("\nMax value is:%d",max);
+	printf("\nIndex is:%d",index);
 	
 	return 0;
 }
